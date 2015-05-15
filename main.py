2f19bae226598ca6a7740147119373f65bc4a27d
Adam <amarinelli@esri.ca>
2015-05-15 11:28:37 -0400

    Refactored code to use Python Standard Library
    - tortilla --> urllib, urllib2

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import urllib
+import urllib2
 import time
 import json
 import os
 
-import tortilla
 import arcpy
 
 timestamp = time.strftime("%Y-%m-%d-%H%M%S")
@@ -13,41 +14,51 @@ class AGOL:
     """ A class for administering an ArcGIS Online account"""
 
     def __init__(self, in_username, in_password, expiration=60):
-        self.agol = tortilla.wrap('https://www.arcgis.com')
-        self.services = tortilla.wrap('https://services.arcgis.com')
+        self.agol = 'https://www.arcgis.com'
+        self.services = 'https://services.arcgis.com'
         self.username = in_username
         self.password = in_password
         self.expiration = expiration
         self.token = self.gen_token()
 
+    def make_request(self, url, params):
+        """ Handles all url requests """
+        
+        data = urllib.urlencode(params)
+        req = urllib2.Request(url, data)
+        return json.loads(urllib2.urlopen(req).read())
+    
     def gen_token(self):
         """ Returns a token given a username and password """
 
-        param = dict(username=self.username, password=self.password, expiration=self.expiration, client='referer',
-                     referer='https://www.arcgis.com', f='json')
+        params = dict(username=self.username,
+                      password=self.password,
+                      expiration=self.expiration,
+                      client='referer',
+                      referer='https://www.arcgis.com',
+                      f='json')
 
-        token = self.agol.sharing.rest.generateToken.post(params=param)
-        # print token.token
+        url = self.agol + '/sharing/rest/generateToken'
+        token = self.make_request(url, params)
 
-        if hasattr(token, 'error'):
-            arcpy.AddMessage(token.error.message)
-            arcpy.AddMessage(token.error.details)
-            quit()
+        if 'error' in token:
+            raise Exception(token['error']['message'], token['error']['details'])
         else:
             return token
 
     def get_user(self):
-        """Returns personal details of the user, such as e-mail and groups,
-        are returned only to the user or the administrator of the user's organization"""
+        """ Returns personal details of the user, such as e-mail and groups,
+        which are returned only to the user or the administrator of the organization """
         
-        param = dict(token=self.token.token, f='json')
+        params = dict(token=self.token['token'],
+                     f='json')
 
-        user = self.agol.sharing.rest.community.users(self.username).get(params=param)
+        url = self.agol + '/sharing/rest/community/users/{}'.format(self.username)
 
-        if hasattr(user, 'error'):
-            arcpy.AddMessage(user.error.message)
-            arcpy.AddMessage(user.error.details)
-            quit()
+        user = self.make_request(url, params)
+
+        if 'error' in user:
+            raise Exception(user['error']['message'], user['error']['details'])
         else:
             return user
         
@@ -55,14 +66,15 @@ class AGOL:
     def get_user_content(self):
         """ Returns the content items for a particular user """
 
-        param = dict(token=self.token.token, f='json')
+        params = dict(token=self.token['token'],
+                     f='json')
 
-        items = self.agol.sharing.rest.content.users(self.username).get(params=param)
+        url = self.agol + '/sharing/rest/content/users/{}'.format(self.username)
 
-        if hasattr(items, 'error'):
-            arcpy.AddMessage(items.error.message)
-            arcpy.AddMessage(items.error.details)
-            quit()
+        items = self.make_request(url, params)
+
+        if 'error' in items:
+            raise Exception(items['error']['message'], items['error']['details'])
         else:
             return items
         
@@ -80,25 +92,30 @@ class AGOL:
                 
         arcpy.CreateFileGDB_management(service_folder, gdb_name, 'CURRENT')
         
-        param = dict(token=self.token.token, f='json')
-        param_query = dict(token=self.token.token, f='json', outFields='*', where='1=1')
+        params = dict(token=self.token['token'],
+                     f='json')
+        params_query = dict(token=self.token['token'],
+                            f='json',
+                            outFields='*',
+                            where='1=1')
         
-        item = self.services(org_id).arcgis.rest.services(name).FeatureServer.get(params=param)
+        url = self.services + '/{}/arcgis/rest/services/{}/FeatureServer'.format(org_id, name)
 
-        for layer in item.layers:
-            layer_item = self.services(org_id).arcgis.rest.services(name).FeatureServer(layer.id).query.get(params=param_query)
+        item = self.make_request(url, params)
 
-            if hasattr(layer_item, 'error'):
-                arcpy.AddMessage(layer_item.error.message)
-                arcpy.AddMessage(layer_item.error.details)
-                quit()
+        for layer in item['layers']:
+            layer_item_url = self.services + '/{}/arcgis/rest/services/{}/FeatureServer/{}/query'.format(org_id, name, layer['id'])
+            layer_item = self.make_request(layer_item_url, params_query)
+
+            if 'error' in layer_item:
+                raise Exception(items['error']['message'], items['error']['details'])
             else:                
-                json_source = os.path.join(json_folder, layer.name + ".json")
+                json_source = os.path.join(json_folder, layer['name'] + ".json")
                 
                 with open(json_source, 'w') as fp:
-                    json_file = json.dump(obj=layer_item, fp=fp)
+                    json_file = json.dump(obj=layer_item, fp=fp, indent=2)
 
-                feature_class_name = ''.join(e for e in layer.name if e.isalnum())
+                feature_class_name = ''.join(e for e in layer['name'] if e.isalnum())
                 output = os.path.join(service_folder, gdb_name, feature_class_name)
                 arcpy.JSONToFeatures_conversion(json_source, output)
         return 
@@ -106,14 +123,14 @@ class AGOL:
     def export_features_bulk(self, org_id, content, export_loc):
         """Exports all feature layers from ArcGIS Online and saves them in a File Geodatabase"""
         
-        items = content.items
+        items = content['items']
         
         for i in items:
-            if i.type == "Feature Service" and "Hosted Service" in i.typeKeywords:
-                arcpy.AddMessage("\nSaving", i.title)
-                arcpy.AddMessage("URL:", i.url)
+            if i['type'] == "Feature Service" and "Hosted Service" in i['typeKeywords']:
+                arcpy.AddMessage("\nSaving", i['title'])
+                arcpy.AddMessage("URL:", i['url'])
                 
-                service = ''.join(e for e in i.title if e.isalnum())
+                service = ''.join(e for e in i['title'] if e.isalnum())
                 service_folder = os.path.join(export_loc, service)
                 os.makedirs(service_folder)
 
@@ -124,24 +141,30 @@ class AGOL:
                 
                 arcpy.CreateFileGDB_management(service_folder, gdb_name, 'CURRENT')
 
-                param = dict(token=self.token.token, f='json')
-                param_query = dict(token=self.token.token, f='json', outFields='*', where='1=1')
-                actual_name = i.url.split("/")[-2]
-                item = self.services(org_id).arcgis.rest.services(actual_name).FeatureServer.get(params=param)
+                params = dict(token=self.token['token'],
+                              f='json')
+                params_query = dict(token=self.token['token'],
+                                    f='json',
+                                    outFields='*',
+                                    where='1=1')
+                
+                actual_name = i['url'].split("/")[-2]
+                url = self.services + '/{}/arcgis/rest/services/{}/FeatureServer'.format(org_id, actual_name)
+                item = self.make_request(url, params)
 
-                for layer in item.layers:
-                    layer_item = self.services(org_id).arcgis.rest.services(actual_name).FeatureServer(layer.id).query.get(params=param_query)
+                for layer in item['layers']:
+                    layer_item_url = self.services + '/{}/arcgis/rest/services/{}/FeatureServer/{}/query'.format(org_id, actual_name, layer['id'])
+                    layer_item = self.make_request(layer_item_url, params_query)
 
-                    if hasattr(layer_item, 'error'):
-                        arcpy.AddMessage(layer_item.error.message)
-                        arcpy.AddMessage(layer_item.error.details)
+                    if 'error' in layer_item:
+                        raise Exception(items['error']['message'], items['error']['details'])
                         pass
                     else:                    
-                        json_source = os.path.join(json_folder, layer.name + ".json")                        
+                        json_source = os.path.join(json_folder, layer['name'] + ".json")                        
                         with open(json_source, 'w') as fp:
-                            json_file = json.dump(obj=layer_item, fp=fp)
+                            json_file = json.dump(obj=layer_item, fp=fp, indent=2)
 
-                        feature_class_name = ''.join(e for e in layer.name if e.isalnum())
+                        feature_class_name = ''.join(e for e in layer['name'] if e.isalnum())
                         output = os.path.join(service_folder, gdb_name, feature_class_name)
                         arcpy.JSONToFeatures_conversion(json_source, output)
         return
@@ -149,11 +172,10 @@ class AGOL:
                     
     def file_writer(self, data, location):
         with open(os.path.join(location, "itemdata-" + timestamp + ".json"), 'w') as fp:
-            json.dump(obj=data, fp=fp, indent=4)
+            json.dump(obj=data, fp=fp, indent=2)
         return
 
-
-if __name__ == "__main__":
+def main():
     
     save_location = arcpy.GetParameterAsText(0)
     username = arcpy.GetParameterAsText(1)
@@ -161,22 +183,35 @@ if __name__ == "__main__":
     batch = arcpy.GetParameterAsText(3)
 
     agol = AGOL(username, password)
+    
+    org_id = agol.get_user()['orgId']
 
-    org_id = agol.get_user().orgId
-
-    backup_location = os.path.join(save_location, timestamp)
-    os.makedirs(backup_location)
+    backup_location = os.path.join(save_location, 'backup')
+    if not os.path.exists(backup_location):
+        os.makedirs(backup_location)
+    session_location = os.path.join(save_location, 'backup', timestamp)
+    os.makedirs(session_location)
     
     content = agol.get_user_content()
-    agol.file_writer(content, backup_location)
+    agol.file_writer(content, session_location)
 
     if batch:
         arcpy.AddMessage("\nBACKUP FEATURE LAYERS")
         arcpy.AddMessage("=====================")
-        agol.export_features_bulk(org_id, agol.get_user_content(), backup_location)
+        agol.export_features_bulk(org_id, agol.get_user_content(), session_location)
     else:
         arcpy.AddMessage("\nEXPORT SINGLE FEATURES")
         arcpy.AddMessage("======================")
-        export = agol.export_features_single(org_id, arcpy.GetParameterAsText(4), backup_location)    
+        export = agol.export_features_single(org_id, arcpy.GetParameterAsText(4), session_location)    
 
     arcpy.AddMessage("\nfinished")
+    print "FINISHED"
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        for error in e:
+            print error
+            arcpy.AddErrpr(error)
